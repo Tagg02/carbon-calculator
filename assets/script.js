1b31fe574d2553f050756733158f3ba8f6517e0b
Tagg02 <harrison.taggart@hotmail.com>
2026-01-02 10:13:59 +0000

    Wire up form inputs and submit handler

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,14 @@
-// Carbon footprint calculator logic will go here
+const form = document.getElementById("carbon-form");
+const result = document.getElementById("result");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const miles = form.miles.value;
+  const kwh = form.kwh.value;
+  const shortFlights = form.shortFlights.value;
+  const longFlights = form.longFlights.value;
+  const diet = form.diet.value;
+
+  console.log(miles, kwh, shortFlights, longFlights, diet);
+});
